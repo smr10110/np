7eff810bd9e0063b9b14f396563692f6baafbb19
smr10110 <a.huaiquil02@ufromail.cl>
2025-11-06 23:47:17 -0300

    delete

--- naive-pay-api/src/main/java/cl/ufro/dci/naivepayapi/dispositivos/dto/CreateDeviceRequest.java
+++
@@ -1,9 +0,0 @@
-package cl.ufro.dci.naivepayapi.dispositivos.dto;
-
-/** Request para crear/registrar un Device. */
-public record CreateDeviceRequest(
-        String fingerprint,
-        String type,
-        String os,
-        String browser
-) {}
--- naive-pay-api/src/main/java/cl/ufro/dci/naivepayapi/registro/dto/SetDynamicKeyDTO.java
+++
@@ -1,9 +0,0 @@
-package cl.ufro.dci.naivepayapi.registro.dto;
-
-import lombok.Data;
-
-@Data
-public class SetDynamicKeyDTO {
-    private String email;
-    private String dynamicKey;
-}
